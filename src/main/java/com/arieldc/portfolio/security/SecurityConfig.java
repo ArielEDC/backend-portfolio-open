63e7daa05e78b52bb0829dc7202d5a5138fb1c2c
Ariel Diaz <adiazcarrenodev@gmail.com>
2025-06-20 13:30:16 -0300

    actualizacion terminada

--- src/main/java/com/arieldc/portfolio/security/SecurityConfig.java
+++ src/main/java/com/arieldc/portfolio/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import com.arieldc.portfolio.security.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -14,6 +15,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -51,9 +53,10 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         return http
-                //.cors(AbstractHttpConfigurer::disable)
-                .csrf(csrf -> csrf.disable())
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authorize) -> {
+                    authorize.requestMatchers(HttpMethod.OPTIONS).permitAll();
                     authorize.requestMatchers(
                             "/auth/**",
                             "/app/persona/ver/**",
@@ -62,7 +65,8 @@ public class SecurityConfig {
                             "/app/proyecto/ver/**",
                             "/app/educacion/ver/**",
                             "/app/experiencia/ver/**",
-                            "/app/contacto/nuevo"
+                            "/app/contacto/nuevo",
+                            "/error"
                             ).permitAll();
                     authorize.anyRequest().authenticated();
                 })
